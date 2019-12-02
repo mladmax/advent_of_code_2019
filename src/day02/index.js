879f79ceb9e0ae0e3347d92de0b57ab89c358819
Mladen Maksimovic <mladen.maximovic@gmail.com>
2019-12-02 20:34:00 +0100

    added day02 puzzle02

--- src/day02/index.js
+++ src/day02/index.js
@@ -7,6 +7,12 @@ const day = __dirname.split('/').pop();
 
 try {
 	log.success(`Result of part one is ${intcode.run(getInput(day), 12, 2)[0]}`);
+
+	for (let noun = 0; noun <= 99; noun++) {
+		for (let verb = 0; verb <= 99; verb++) {
+			if (intcode.run(getInput(day), noun, verb)[0] === 19690720) log.success(`Result of part two is ${100 * noun + verb}`);
+		}
+	}
 } catch (error) {
 	log.error(error.message);
 }
