d4ece9c5a8b2b3591f6824d13139803ca980f144
Mladen Maksimovic <mladen.maximovic@gmail.com>
2019-12-02 22:21:25 +0100

    added code

--- src/day02/index.js
+++ src/day02/index.js
@@ -4,13 +4,15 @@ const numbers = require('../utils/numbers');
 
 const intcode = require('./intcode');
 
-const day = __dirname.split('/').pop();
+const day = __dirname.split('\\').pop();
 
 try {
-	log.success(`Result of part one is ${intcode.run(input.get(day), 12, 2)[0]}`);
+	const code = input.get(day);
+
+	log.success(`Result of part one is ${intcode.run(code, 12, 2)[0]}`);
 
 	numbers.combin2(0, 99).forEach(combination => {
-		if (intcode.run(input.get(day), combination[0], combination[1])[0] === 19690720) {
+		if (intcode.run(code, combination[0], combination[1])[0] === 19690720) {
 			log.success(`Result of part two is ${100 * combination[0] + combination[1]}`);
 		}
 	});
